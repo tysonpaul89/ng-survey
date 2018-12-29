a2dee83a637a4f142f2b10cf986c2a146b0403aa
Tyson <tysonpaul89@gmail.com>
2018-12-29 23:03:35 +0530

    Tab UI logic added

--- src/app/layout/survey-builder/builder-page/builder-page.component.ts
+++ src/app/layout/survey-builder/builder-page/builder-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgbRatingConfig, NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 
+import { ITab } from '../../../shared/interface/ITab';
 
 @Component({
   selector: 'app-builder-page',
@@ -9,6 +10,8 @@ import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./builder-page.component.scss']
 })
 export class BuilderPageComponent implements OnInit {
+  private idCounter = 1;
+  public tabs: ITab[] = [];
 
   constructor(
     private modalService: NgbModal,
@@ -19,6 +22,11 @@ export class BuilderPageComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.tabs.push({
+      id: 'page-' + this.getTabId(),
+      title: 'Page ' + this.idCounter,
+      content: null
+    });
   }
 
   /**
@@ -27,6 +35,42 @@ export class BuilderPageComponent implements OnInit {
    */
   openSurveyInputEditModal(content) {
     this.modalService.open(content, { size: 'sm' });
+  }
+
+  /**
+   * To add a new tab
+   */
+  addNewTab() {
+    ++this.idCounter;
+
+    this.tabs = this.tabs.concat({
+      id: 'page-' + this.getTabId(),
+      title: 'Page ' + this.idCounter,
+      content: null
+    });
+  }
+
+  /**
+   * To delete a tab
+   * @param tabId Tab id
+   */
+  deleteTab(tabId, $event) {
+    $event.preventDefault();
+    const result: ITab[] = this.tabs.filter(
+      (tab: ITab): boolean => tab.id !== tabId
+    );
+    result.map(
+      (tab: ITab, i: number) => tab.title = 'Page ' + (i + 1)
+    );
+    this.tabs = result;
+  }
+
+  /**
+   * Generate a unique integer
+   * @returns number
+   */
+  getTabId() {
+    return new Date().getTime();
   }
 
 }
