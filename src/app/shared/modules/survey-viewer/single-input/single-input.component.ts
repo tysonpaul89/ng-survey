b6ab645627a4fe13ee708f6ad2e071dd1debd904
Tyson <tysonpaul89@gmail.com>
2018-12-30 13:45:55 +0530

    survey input UI elements splited into components

---
+++ src/app/shared/modules/survey-viewer/single-input/single-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-input',
+  templateUrl: './single-input.component.html',
+  styleUrls: ['./single-input.component.scss']
+})
+export class SingleInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/modules/survey-viewer/survey-viewer.module.ts
+++ src/app/shared/modules/survey-viewer/survey-viewer.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewerComponent } from './viewer/viewer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SingleInputComponent } from './single-input/single-input.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+import { MultipleChoiceComponent } from './multiple-choice/multiple-choice.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { CommentComponent } from './comment/comment.component';
+import { RatingComponent } from './rating/rating.component';
 
 @NgModule({
-  declarations: [ViewerComponent],
+  declarations: [ViewerComponent, SingleInputComponent, CheckboxComponent, MultipleChoiceComponent, DropdownComponent, CommentComponent, RatingComponent],
   imports: [
     CommonModule,
     NgbModule,
